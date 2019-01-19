5ce7e3622735ed13086edbf3989e31ee8698eb7a
horsti58 <horsti58@gmx.netm>
2019-01-19 13:08:29 +0100

    update

---
+++ scripte/script_all.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cd ~/Settings/scripte
+
+./script_28.2E_26.0E_23.5E_19.2E_16.0E_13.0E_9.0E_7.0E_4.8E_0.8W.sh
+echo "make 28.2E_26.0E_23.5E_19.2E_16.0E_13.0E_9.0E_7.0E_4.8E_0.8W"
+sleep 2
+
+./script_28.2E_23.5E_19.2E_13.0E_9.0E_4.8E_0.8W.sh
+echo "make 28.2E_23.5E_19.2E_13.0E_9.0E_4.8E_0.8W"
+sleep 2
+
+./script_19.2E_13.0E_28.2E.sh
+echo "make 19.2E_13.0E_28.2E"
+sleep 2
+
+./script_19.2E_13.0E_23.5E_28.2E_9.0E.sh
+echo "make 19.2E_13.0E_23.5E_28.2E_9.0E"
+sleep 2
+
+./script_19.2E_13.0E_23.5E_28.2E.sh
+echo "make 19.2E_13.0E_23.5E_28.2E"
+sleep 2
+
+./script_19.2E_13.0E_23.5E.sh
+echo "make 19.2E_13.0E_23.5E"
+sleep 2
+
+./script_19.2E_13.0E.sh
+echo "make 19.2E_13.0E"
+sleep 2
+
+./script_19.2E.sh
+echo "make 19.2E"
+sleep 2
+
