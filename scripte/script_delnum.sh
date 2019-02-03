deb342f40444e49a29685a687887bcff6b096030
horsti58 <horsti58@gmx.netm>
2019-02-03 14:02:36 +0100

    Update Settings 2019-02-03

---
+++ scripte/script_delnum.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+xsltproc -o /home/annie/settings/19.2E/services.onum.xml ./delnum.xsl /home/annie/settings/19.2E/services.xml
+echo "19.2E done"
+xsltproc -o /home/annie/settings/19.2E_13.0E/services.onum.xml ./delnum.xsl /home/annie/settings/19.2E_13.0E/services.xml
+echo "19.2E_13.0E done"
+xsltproc -o /home/annie/settings/19.2E_13.0E_23.5E/services.onum.xml ./delnum.xsl /home/annie/settings/19.2E_13.0E_23.5E/services.xml
+echo "19.2E_13.0E_23.5E done"
+xsltproc -o /home/annie/settings/19.2E_13.0E_23.5E_28.2E/services.onum.xml ./delnum.xsl /home/annie/settings/19.2E_13.0E_23.5E_28.2E/services.xml
+echo "19.2E_13.0E_23.5E_28.2E done"
+xsltproc -o /home/annie/settings/19.2E_13.0E_23.5E_28.2E_9.0E/services.onum.xml ./delnum.xsl /home/annie/settings/19.2E_13.0E_23.5E_28.2E_9.0E/services.xml
+echo "19.2E_13.0E_23.5E_28.2E_9.0E done"
+xsltproc -o /home/annie/settings/19.2E_13.0E_28.2E/services.onum.xml ./delnum.xsl /home/annie/settings/19.2E_13.0E_28.2E/services.xml
+echo "19.2E_13.0E_28.2E done"
+xsltproc -o /home/annie/settings/28.2E_23.5E_19.2E_13.0E_9.0E_4.8E_0.8W/services.onum.xml ./delnum.xsl /home/annie/settings/28.2E_23.5E_19.2E_13.0E_9.0E_4.8E_0.8W/services.xml
+echo "28.2E_23.5E_19.2E_13.0E_9.0E_4.8E_0.8W done"
+xsltproc -o /home/annie/settings/28.2E_26.0E_23.5E_19.2E_16.0E_13.0E_9.0E_7.0E_4.8E_0.8W/services.onum.xml ./delnum.xsl /home/annie/settings/28.2E_26.0E_23.5E_19.2E_16.0E_13.0E_9.0E_7.0E_4.8E_0.8W/services.xml
+echo "28.2E_26.0E_23.5E_19.2E_16.0E_13.0E_9.0E_7.0E_4.8E_0.8W done"
+
