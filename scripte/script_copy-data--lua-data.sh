3095fc75c6f06960598b89230e6bb1cdf951e0c4
horsti58 <horsti58@gmx.netm>
2019-02-15 13:38:14 +0100

    update settings 2019-02-15

---
+++ scripte/script_copy-data--lua-data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd ~/Settings/data
+
+cp -f -R ~/Settings/data/* ~/lua-data/
+
+
+
---
+++ scripte/script_del-data.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+rm /home/annie/settings/data/0.8W/*.xml
+echo "clear data/0.8W done"
+rm /home/annie/settings/data/4.8E/*.xml
+echo "clear data/4.8E done"
+rm /home/annie/settings/data/7.0E/*.xml
+echo "clear data/7.0E done"
+rm /home/annie/settings/data/9.0E/*.xml
+echo "clear data/9.0E done"
+rm /home/annie/settings/data/13.0E/*.xml
+echo "clear data/13.0E done"
+rm /home/annie/settings/data/16.0E/*.xml
+echo "clear data/16.0E done"
+rm /home/annie/settings/data/19.2E/*.xml
+echo "clear data/19.2E done"
+rm /home/annie/settings/data/23.5E/*.xml
+echo "clear data/23.5E done"
+rm /home/annie/settings/data/26.0E/*.xml
+echo "clear data/26.0E done"
+rm /home/annie/settings/data/28.2E/*.xml
+echo "clear data/28.2E done"
+
+
